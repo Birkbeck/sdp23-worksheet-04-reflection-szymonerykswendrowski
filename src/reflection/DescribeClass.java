8c699912aebf14ee1f3ee1460c0701ef89f25aaf
Szymon Eryk Swendrowski <szymon.swendrowski@gmail.com>
2023-02-14 17:19:57 +0000

    Modified exercise 1 to use method references instead.

--- src/reflection/DescribeClass.java
+++ src/reflection/DescribeClass.java
@@ -8,35 +8,66 @@ import java.util.Arrays;
 public class DescribeClass {
     public static void main(String[] args) throws ClassNotFoundException {
         if (args.length  > 0) {
-            System.out.println("Usage: "); // TODO: describe how to use the utility
 
             // Read the name of the class
-            Class<?> aClass = Class.forName(args[0]);
+            DescribeClass aClass = new DescribeClass(args[0]);
             System.out.println("The name of the class is " + aClass.getName());
 
             // Read the interfaces implemented by the class
-            Class<?>[] interfaces = aClass.getInterfaces();
-            System.out.println("The interfaces implemented by " + aClass.getName() + " are: " + Arrays.toString(interfaces));
+            System.out.println("The interfaces implemented by " + aClass.getName() + " are: " + Arrays.toString(aClass.getInterfaces()));
 
             // Read the super class of the class
-            Class<?> superClass = aClass.getSuperclass();
-            System.out.println("The super class of " + aClass.getName() + " is " + superClass.getName());
+            System.out.println("The super class of " + aClass.getName() + " is " + aClass.getSuperClass());
 
             // Read the methods of the class
-            Method[] methods = aClass.getMethods();
-            System.out.println("The methods of " + aClass.getName() + " are: " + Arrays.toString(methods));
+            System.out.println("The methods of " + aClass.getName() + " are: " + Arrays.toString(aClass.getMethods()));
 
             // Read the modifiers of the methods of the class
-            Arrays.stream(methods).map(method -> "The modifiers of method " + method.getName() + " are: " + Modifier.toString(method.getModifiers())).forEach(System.out::println);
+            aClass.methodModifiers(aClass.getMethods());
 
             // Read the constructors of the class
-            Constructor<?>[] constructors = aClass.getConstructors();
-            System.out.println("The constructors of the class are: " + Arrays.toString(constructors));
+            System.out.println("The constructors of the class are: " + Arrays.toString(aClass.getConstructors()));
 
             // Read the fields of the class
-            Class<?>[] fields = aClass.getClasses();
-            System.out.println("The fields of " + aClass.getName() + " are: " + Arrays.toString(fields));
-
+            System.out.println("The fields of " + aClass.getName() + " are: " + Arrays.toString(aClass.getFields()));
         }
+    }
+
+    private Class<?> c;
+
+    public DescribeClass(Class<?> c) {
+        this.c = c;
+    }
+
+    public DescribeClass(String className) throws ClassNotFoundException {
+        this.c = Class.forName(className);
+    }
+
+    public String getName() {
+        return c.getName();
+    }
+
+    public Class<?>[] getInterfaces() {
+        return c.getInterfaces();
+    }
+
+    public Class<?> getSuperClass() {
+        return c.getSuperclass();
+    }
+
+   public Method[] getMethods() {
+        return c.getMethods();
+    }
+
+    public void methodModifiers(Method[] methods) {
+        Arrays.stream(methods).map(method -> "The modifiers of method " + method.getName() + " are: " + Modifier.toString(c.getModifiers())).forEach(System.out::println);
+    }
+
+    public Constructor<?>[] getConstructors() {
+        return c.getConstructors();
+    }
+
+    public Class<?>[] getFields() {
+        return c.getClasses();
     }
 }
